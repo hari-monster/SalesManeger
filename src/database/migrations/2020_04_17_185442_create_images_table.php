5288bbc993c52597e49d283535d62d6fedf25636
ryunosuke-ishihara <sp.ryunosuke1@gmail.com>
2020-05-19 18:18:37 +0900

    2020_04_17_185442_create_images_table.phpの　up編集

--- src/database/migrations/2020_04_17_185442_create_images_table.php
+++ src/database/migrations/2020_04_17_185442_create_images_table.php
@@ -14,7 +14,7 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->comment('ID');
             $table->timestamps();
         });
     }
