89c3766f52d638104d76275f1ddad554458d08a0
MurayamaKenta <imken924@gmail.com>
2020-06-10 02:05:18 +0900

    シーダーの追加 ファクトリーの修正

--- src/database/factories/ProductTranslationFactory.php
+++ src/database/factories/ProductTranslationFactory.php
@@ -2,11 +2,18 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Product;
 use App\Model\ProductTranslation;
 use Faker\Generator as Faker;
 
 $factory->define(ProductTranslation::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function () {
+            return factory(Product::class)->create()->id;
+        },
+        'name' => $faker->text(10),
+        'local' => $faker->uuid,
+        'description' => $faker->paragraph,
     ];
-});
+}
+);
